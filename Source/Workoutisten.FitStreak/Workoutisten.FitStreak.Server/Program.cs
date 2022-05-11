f82bb06ea2327bfc1e86cbe04cf1bb9f4ef557a2
FlorianAlbert <florian21albert@gmx.de>
2022-05-11 12:10:34 +0200

    Added Triggers

--- Source/Workoutisten.FitStreak/Workoutisten.FitStreak.Server/Program.cs
+++ Source/Workoutisten.FitStreak/Workoutisten.FitStreak.Server/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Workoutisten.FitStreak.Server.Database;
 using Workoutisten.FitStreak.Server.Database.Implementation;
 using Workoutisten.FitStreak.Server.Database.Interface;
+using Workoutisten.FitStreak.Server.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,9 @@ builder.Services.AddDbContext<FitStreakDbContext>(options =>
         optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(FitStreakDbContext).Assembly.FullName));
     options.UseTriggers();
 });
+
+// Add Triggers to the container
+builder.Services.AddTriggers();
 
 // Add own services to the container
 builder.Services.AddScoped<IRepository, Repository>();
