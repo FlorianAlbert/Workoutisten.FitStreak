ede47e9b908d94bff8670359145e18dff07670a1
FlorianAlbert <florian21albert@gmx.de>
2022-05-16 14:26:52 +0200

    Corrected Declaration of Methods in PasswordService

--- Source/Workoutisten.FitStreak/Workoutisten.FitStreak.Server.Service.Implementation/UserManagement/PasswordService.cs
+++ Source/Workoutisten.FitStreak/Workoutisten.FitStreak.Server.Service.Implementation/UserManagement/PasswordService.cs
@@ -1,19 +1,20 @@
-﻿using Workoutisten.FitStreak.Server.Service.Interface.UserManagement;
+﻿using Workoutisten.FitStreak.Server.Service.Interface.Data;
+using Workoutisten.FitStreak.Server.Service.Interface.UserManagement;
 
 namespace Workoutisten.FitStreak.Server.Service.Implementation.UserManagement;
 public class PasswordService : IPasswordService
 {
-    public async Task<bool> ChangePasswordAsync(Guid userId, string oldPassword, string newPassword)
+    public async Task<Result<bool>> ChangePasswordAsync(Guid userId, string oldPassword, string newPassword)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<bool> ConfirmPasswordResetAsync(Guid passwordForgottenKey, string newPassword)
+    public async Task<Result<bool>> ConfirmPasswordResetAsync(Guid passwordForgottenKey, string newPassword)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<bool> RequestPasswordResetAsync(string email)
+    public async Task<Result<bool>> RequestPasswordResetAsync(string email)
     {
         throw new NotImplementedException();
     }
