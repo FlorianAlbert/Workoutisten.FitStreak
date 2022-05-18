d3e67a70120688d81995933429387d39775ad0b6
Leon-Jerke <62246987+Leon-Jerke@users.noreply.github.com>
2022-05-18 02:10:20 +0200

    Changed InputFields of Login and SignUp

--- Source/Workoutisten.FitStreak/Workoutisten.FitStreak/Data/Models/User/RegisterModel.cs
+++ Source/Workoutisten.FitStreak/Workoutisten.FitStreak/Data/Models/User/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,17 @@ namespace Workoutisten.FitStreak.Data.Models.User
 {
     public class RegisterModel
     {
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
 
+        [Required]
+        [EmailAddress(ErrorMessage = "The email address is invalid")]
         public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
